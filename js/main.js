3e2d3c90af2bc36398983de7b1402966b0952201
dianaonrails <d.r.carvalho89@gmail.com>
2017-09-19 00:05:18 +0100

    js menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 $(document).ready(function () {  
   var top = $('.header-menu').offset().top;
-  $(window).scroll(function (event) {
-    // what the y position of the scroll is
-    var y = $(this).scrollTop();
-
-    // whether that's below the form
-    if (y >top) {
-      // if so, ad the fixed class
-      $('.header-menu').addClass('background-menu');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 125) {
+        $('.header-menu').addClass('background-menu');
     } else {
-      // otherwise remove it
-      $('.header-menu').removeClass('background-menu');
+        $('.header-menu').removeClass('background-menu');
     }
+
   });
 });
