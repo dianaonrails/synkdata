ed11732a9dacc50c3d045cd413deca0129ffeae2
dianaonrails <d.r.carvalho89@gmail.com>
2017-09-18 23:22:37 +0100

    floating menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-  $('.carousel').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
+$(document).ready(function () {  
+  var top = $('.header-menu').offset().top - parseFloat($('.header-menu').css('marginTop').replace(/auto/, 100));
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >=top) {
+      // if so, ad the fixed class
+      $('.header-menu').addClass('background-menu');
+    } else {
+      // otherwise remove it
+      $('.header-menu').removeClass('background-menu');
+    }
   });
 });
